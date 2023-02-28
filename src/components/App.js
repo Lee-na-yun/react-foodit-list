e6cb502940c4db72ae17e2ff689092593feb2fe5
Lee-na-yun <happyin3333@nate.com>
2023-02-28 11:17:14 +0900

    filter로 item 삭제하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import FoodList from "./FoodList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
-    const [order, setOrder] = useState("createdAt");
-    const sortedItems = items.sort((a,b)=>b[order]-a[order]);
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-    const handleNewClick = () => setOrder("createdAt");
-    const handleCalClick = () => setOrder("calorie");
+  const handleNewClick = () => setOrder("createdAt");
+  const handleCalClick = () => setOrder("calorie");
+  const handleDelClick = (id) => {
+    const delItem = items.filter((item) => item.id !== id);
+    setItems(delItem);
+  };
 
   return (
     <div>
-        <button onClick={handleNewClick}>최신순</button>
-        <button onClick={handleCalClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <button onClick={handleNewClick}>최신순</button>
+      <button onClick={handleCalClick}>칼로리순</button>
+      <FoodList items={sortedItems} onDelete={handleDelClick}/>
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,12 +1,13 @@
 import "./FoodList.css";
 
 function formatDate(value) {
-    const date = new Date(value);
-    return `${date.getFullYear()}, ${date.getMonth() + 1}, ${date.getDate()}`;
-  }
+  const date = new Date(value);
+  return `${date.getFullYear()}, ${date.getMonth() + 1}, ${date.getDate()}`;
+}
 
-function FoodListItem({ item }) {
+function FoodListItem({ item, onDelete }) {
   const { imgUrl, title, calorie, content } = item;
+  const handleDeleteItem = () => onDelete(item.id);
 
   return (
     <div className="FoodListItem">
@@ -15,19 +16,20 @@ function FoodListItem({ item }) {
         <h3>{title}</h3>
         <p>{calorie}</p>
         <p>{content}</p>
-        <p>{formatDate(item.createdAt)}</p>{" "}
+        <p>{formatDate(item.createdAt)}</p>
+        <button onClick={handleDeleteItem}>삭제</button>
       </div>
     </div>
   );
 }
 
-function FoodList({ items }) {
+function FoodList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
         return (
           <li>
-            <FoodListItem item={item} />
+            <FoodListItem item={item} onDelete={onDelete} />
           </li>
         );
       })}
