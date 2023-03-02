2dd11050c0a7fba9fb96dbef7938cb29fed788ab
Lee-na-yun <happyin3333@nate.com>
2023-03-02 10:55:14 +0900

    fetch함수로 네트워크 데이터 불러오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import FoodList from "./FoodList";
-import mockItems from "../mock.json";
+//import mockItems from "../mock.json";
 import { useState } from "react";
+import { getFoods } from "../api";
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -14,11 +15,17 @@ function App() {
     setItems(delItem);
   };
 
+  const handleLoadClick = async () => {
+    const {foods} = await getFoods();
+    setItems(foods);
+  }
+
   return (
     <div>
       <button onClick={handleNewClick}>최신순</button>
       <button onClick={handleCalClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelClick}/>
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
