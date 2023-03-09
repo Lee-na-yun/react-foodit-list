d451ad9df4a970ddf3b8446590e0e713e66da9ee
Lee-na-yun <happyin3333@nate.com>
2023-03-09 14:07:34 +0900

    cursor로 페이지네이션 기능 추가하기

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
-export async function getFoods(order = '') {
-    const query = `order=${order}`;
+export async function getFoods({order = '', cursor='', limit=10}) {
+    const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
     const response = await fetch(`https://learn.codeit.kr/7008/foods?${query}`);
     const body = await response.json();
     return body;
 }
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,55 @@
-import FoodList from "./FoodList";
-//import mockItems from "../mock.json";
-import { useEffect, useState } from "react";
-import { getFoods } from "../api";
+import { useEffect, useState } from 'react';
+import { getFoods } from '../api';
+import FoodList from './FoodList';
 
 function App() {
+  const [order, setOrder] = useState('createdAt');
+  const [cursor, setCursor] = useState(null);
   const [items, setItems] = useState([]);
-  const [order, setOrder] = useState("createdAt");
+
+  const handleNewestClick = () => setOrder('createdAt');
+
+  const handleCalorieClick = () => setOrder('calorie');
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
+  };
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  const handleNewClick = () => setOrder("createdAt");
-  const handleCalClick = () => setOrder("calorie");
-  const handleDelClick = (id) => {
-    const delItem = items.filter((item) => item.id !== id);
-    setItems(delItem);
-  };
-
-  const handleLoad = async (orderQuery) => {
-    const {foods} = await getFoods(orderQuery);
-    setItems(foods);
-  };
-
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({
+      order,
+    });
   }, [order]);
 
   return (
     <div>
-      <button onClick={handleNewClick}>최신순</button>
-      <button onClick={handleCalClick}>칼로리순</button>
-      <FoodList items={sortedItems} onDelete={handleDelClick}/>
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
